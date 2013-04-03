9dc23b1970e5e80caf2cf737663037f2fdcb2a5c
JustinKellogg <jdk998@mst.edu>
2013-04-03 17:19:49 -0500

    added generic views and test cases

--- sodaSite/polls/models.py
+++ sodaSite/polls/models.py
@@ -9,9 +9,12 @@ class Poll(models.Model):
     def __str__(self):
         return self.question
     def was_published_recently(self):
-        return self.pub_date >= timezone.now()- datetime.timedelta(days=1)
+        return timezone.now()- datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
+
+
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
