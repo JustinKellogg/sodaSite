d8e5ff7f94ba466298e5f0181c3d646df4305cb8
JustinKellogg <jdk998@mst.edu>
2013-04-27 14:13:16 -0500

    starting on chart app

---
+++ sodaSite/chart/views.py
@@ -0,0 +1,31 @@
+# Create your views here.
+from sodaSite.api.models import *
+from django.http import HttpResponse
+from django.utils import simplejson, timezone
+from pychart import *
+from django.template import loader, Context
+from django.shortcuts import render_to_response
+
+
+def example(request):
+    return render_to_response("chart/example.html")
+
+
+def salesByType(request):
+   # dewSales = SodaTransaction.objects.filter(Soda__name=soda_type)
+    data = {'data' : }
+    return render_to_response("chart/example.html")
+
+
+def salesByTime(request):
+    pass
+
+
+def salesByMachine(request):
+    pass
+
+
+def salesByBuilding(request):
+    pass
+
+
