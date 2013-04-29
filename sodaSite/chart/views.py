7e1ed7b5b4472862a6bf85fa305f745a3567c688
JustinKellogg <jdk998@mst.edu>
2013-04-29 18:52:03 -0500

    generate graphs based on data from view

--- sodaSite/chart/views.py
+++ sodaSite/chart/views.py
@@ -3,22 +3,29 @@ from sodaSite.api.models import *
 from django.http import HttpResponse
 from sodaSite.api.sodaTypes import SODA_TYPE_CHOICES, TYPE_DICT
 from django.utils import simplejson, timezone
-from pychart import *
-from django.template import loader, Context
 from django.shortcuts import render_to_response
 
 
 def example(request):
-    return render_to_response("chart/example.html")
+    sales = {'Mushrooms': 55, 'Onions': 13, 'Olives': 15, 'Zucchini': 31, 'Pepperoni': 2, 'Cheese': 44}
+    sales = simplejson.dumps(sales)
+    return render_to_response("chart/example.html", {'sales': sales})
 
 
 def salesByType(request):
-    dewSales = SodaTransaction.objects.filter(Soda__name=TYPE_DICT[1])
-    data = {'data': 'stuff'}
-    return render_to_response("chart/example.html")
+    dewSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[1]))
+    docSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[2]))
+    pepSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[3]))
+    cokSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[4]))
+    sales = {'dew': dewSales, 'doc': docSales, 'pep': pepSales, 'coke': cokSales}
+    sales = simplejson.dumps(sales)
+    return render_to_response("chart/example.html", {'sales': sales})
 
 
 def salesByTime(request):
+    data = (('IO on eyedrops', 61), ('Haskel on Hovercrafts', 276),
+            ('Lua on Linseed Oil', 99), ('Django', 1000))
+    dataset = [(item[0],[[0,item[1]]]) for item in data]
     pass
 
 
