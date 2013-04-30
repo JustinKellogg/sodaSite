b44fe9a1ed0ab7edaa04f48dd257dee5a70804a3
JustinKellogg <jdk998@mst.edu>
2013-04-30 07:25:19 -0500

    renamed html file, renamed variable in javascript, dynamic title in graphs

--- sodaSite/chart/views.py
+++ sodaSite/chart/views.py
@@ -7,25 +7,23 @@ from django.shortcuts import render_to_response
 
 
 def example(request):
-    sales = {'Mushrooms': 55, 'Onions': 13, 'Olives': 15, 'Zucchini': 31, 'Pepperoni': 2, 'Cheese': 44}
+    sales = {'Title': 'pizza example', 'Mushrooms': 55, 'Onions': 13, 'Olives': 15, 'Zucchini': 31, 'Pepperoni': 2, 'Cheese': 44}
     sales = simplejson.dumps(sales)
-    return render_to_response("chart/example.html", {'sales': sales})
+    return render_to_response("chart/pieChart.html", {'sales': sales})
 
 
 def salesByType(request):
+    title = "Sales by Type"
     dewSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[1]))
     docSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[2]))
     pepSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[3]))
     cokSales = len(SodaTransaction.objects.filter(Soda__name=TYPE_DICT[4]))
-    sales = {'dew': dewSales, 'doc': docSales, 'pep': pepSales, 'coke': cokSales}
+    sales = {'Title': title, 'Mountain Dew': dewSales, 'Doctor Pepper': docSales, 'Pepsi': pepSales, 'Coke': cokSales}
     sales = simplejson.dumps(sales)
-    return render_to_response("chart/example.html", {'sales': sales})
+    return render_to_response("chart/pieChart.html", {'sales': sales})
 
 
 def salesByTime(request):
-    data = (('IO on eyedrops', 61), ('Haskel on Hovercrafts', 276),
-            ('Lua on Linseed Oil', 99), ('Django', 1000))
-    dataset = [(item[0],[[0,item[1]]]) for item in data]
     pass
 
 
