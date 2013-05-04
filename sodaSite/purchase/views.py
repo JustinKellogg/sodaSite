0dffae504d783653be558700640a7528bafb99c0
jerry <ghrbkramer@gmail.com>
2013-05-04 15:53:25 -0500

    fixed error submitting purchase w/o picking soda

--- sodaSite/purchase/views.py
+++ sodaSite/purchase/views.py
@@ -40,34 +40,41 @@ def my_logout(request):
   context_instance = RequestContext(request))
   
 def buy(request):
-  slotid = request.POST['slot']
-  slot = InventorySlot.objects.get(pk=slotid)
   muser = request.user.machineuser_set.all()
   muser = muser[0]
-  soda = slot.soda_set.all()
-  soda = soda[0]
-  desc = "User %i bought a %s at " % (muser.studentID, soda.name) + str(datetime.now())
-  if muser.funds >= soda.cost:
-    muser.funds -= soda.cost
-    muser.save()
-    slot.amount -= 1
-    slot.save()
-    slot.Machine.lastContact = datetime.now()
-    slot.Machine.save(update_fields=['lastContact'])
-    if slot.amount < 1:
-        send_mail('Out of Stock', "Slot %i of Machine %i has %i sodas left" % (slot.id, slot.Machine.id, slot.amount),
-                 'sodaacm@gmail.com', [admin[1] for admin in ADMINS])
-    trans = SodaTransaction.objects.create(amount=soda.cost, date_time=datetime.now(), description=desc,
-                User=muser, Soda=soda)
+  try:
+    slotid = request.POST['slot']
+    slot = InventorySlot.objects.get(pk=slotid)
+  except (KeyError, InventorySlot.DoesNotExist):
     return render_to_response('purchase/index.html',
     {'user': muser, 'machine_list': Machine.objects.all(),
-    'purchase': "Soda successfully purchased"},
+    'purchase': "You didn't choose anything."},
     context_instance = RequestContext(request))
   else:
-    return render_to_response('purchase/index.html',
-    {'user': muser, 'machine_list': Machine.objects.all(),
-    'purchase': "You don't have enough money"},
-    context_instance = RequestContext(request))
+    soda = slot.soda_set.all()
+    soda = soda[0]
+    desc = "User %i bought a %s at " % (muser.studentID, soda.name) + str(datetime.now())
+    if muser.funds >= soda.cost:
+      muser.funds -= soda.cost
+      muser.save()
+      slot.amount -= 1
+      slot.save()
+      slot.Machine.lastContact = datetime.now()
+      slot.Machine.save(update_fields=['lastContact'])
+      if slot.amount < 1:
+          send_mail('Out of Stock', "Slot %i of Machine %i has %i sodas left" % (slot.id, slot.Machine.id, slot.amount),
+                   'sodaacm@gmail.com', [admin[1] for admin in ADMINS])
+      trans = SodaTransaction.objects.create(amount=soda.cost, date_time=datetime.now(), description=desc,
+                  User=muser, Soda=soda)
+      return render_to_response('purchase/index.html',
+      {'user': muser, 'machine_list': Machine.objects.all(),
+      'purchase': "Soda successfully purchased"},
+      context_instance = RequestContext(request))
+    else:
+      return render_to_response('purchase/index.html',
+      {'user': muser, 'machine_list': Machine.objects.all(),
+      'purchase': "You don't have enough money"},
+      context_instance = RequestContext(request))
   
   
   
