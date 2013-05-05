fbfa6a9a434bb5dab4cf02a95d518758dcdffbd9
jerry <ghrbkramer@gmail.com>
2013-05-04 21:43:06 -0500

    added tests

--- sodaSite/api/views.py
+++ sodaSite/api/views.py
@@ -7,42 +7,42 @@ from django.utils import simplejson, timezone
 from sodaSite.settings import ADMINS
 
 
-def transactions(request, soda_id, stid):
-    #TODO: Check that request comes from raspberry pi's
-    soda = Soda.objects.get(id=soda_id)
-    try:
-        user = MachineUser.objects.get(studentID=stid)
-    except:
-        #TODO narrow down exception
-        ip = request.META.get('HTTP_X_FORWARDED_FOR')
-        ip = request.META.get('REMOTE_HOST')
-        ip = request.user
-        response = {'result': 'failure', 'error': 'Student id does not exist '+ str(ip)}
-        return HttpResponse(simplejson.dumps(response), mimetype='application/json')
-    slot = soda.Slot
-    desc = "User %i bought a %s at " % (user.studentID, soda.name) + str(datetime.now())
-    if user.funds >= soda.cost:
-        if slot.amount > 0:
-            user.funds -= soda.cost
-            user.save()
-            slot.amount -= 1
-            slot.save()
-            slot.Machine.lastContact = datetime.now()
-            slot.Machine.save(update_fields=['lastContact'])
-            if slot.amount < 1:
-                send_mail('Out of Stock', "Slot %i of Machine %i has %i sodas left" % (slot.id, slot.Machine.id, slot.amount),
-                         'sodaacm@gmail.com', ['jdk998@mst.edu'])
-            trans = SodaTransaction(amount=soda.cost, date_time=datetime.now(), description=desc,
-                        User=user, Soda=soda)
-            trans.save()
-            resp = {'result': 'Success', 'error': 'None'}
-            return HttpResponse(simplejson.dumps(resp), mimetype='application/json')
-        else:
-            resp = {'result': 'Failure', 'error': 'Out of inventory'}
-            return HttpResponse(simplejson.dumps(resp), mimetype='application/json')
-    else:
-        resp = {'result': 'Failure', 'error': 'Out of Money'}
-        return HttpResponse(simplejson.dumps(resp), mimetype='application/json')
+# def transactions(request, soda_id, stid):
+    # TODO: Check that request comes from raspberry pi's
+    # soda = Soda.objects.get(id=soda_id)
+    # try:
+        # user = MachineUser.objects.get(studentID=stid)
+    # except:
+        # TODO narrow down exception
+        # ip = request.META.get('HTTP_X_FORWARDED_FOR')
+        # ip = request.META.get('REMOTE_HOST')
+        # ip = request.user
+        # response = {'result': 'failure', 'error': 'Student id does not exist '+ str(ip)}
+        # return HttpResponse(simplejson.dumps(response), mimetype='application/json')
+    # slot = soda.Slot
+    # desc = "User %i bought a %s at " % (user.studentID, soda.name) + str(datetime.now())
+    # if user.funds >= soda.cost:
+        # if slot.amount > 0:
+            # user.funds -= soda.cost
+            # user.save()
+            # slot.amount -= 1
+            # slot.save()
+            # slot.Machine.lastContact = datetime.now()
+            # slot.Machine.save(update_fields=['lastContact'])
+            # if slot.amount < 1:
+                # send_mail('Out of Stock', "Slot %i of Machine %i has %i sodas left" % (slot.id, slot.Machine.id, slot.amount),
+                         # 'sodaacm@gmail.com', ['jdk998@mst.edu'])
+            # trans = SodaTransaction(amount=soda.cost, date_time=datetime.now(), description=desc,
+                        # User=user, Soda=soda)
+            # trans.save()
+            # resp = {'result': 'Success', 'error': 'None'}
+            # return HttpResponse(simplejson.dumps(resp), mimetype='application/json')
+        # else:
+            # resp = {'result': 'Failure', 'error': 'Out of inventory'}
+            # return HttpResponse(simplejson.dumps(resp), mimetype='application/json')
+    # else:
+        # resp = {'result': 'Failure', 'error': 'Out of Money'}
+        # return HttpResponse(simplejson.dumps(resp), mimetype='application/json')
 
 
 def ping_machine(request, m_id):
--- sodaSite/purchase/tests.py
+++ sodaSite/purchase/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from sodaSite.api.models import Machine, InventorySlot, MachineUser, Soda, Transaction, SodaTransaction
+from datetime import datetime
+from django.contrib.auth.models import User
 
+class PurchaseMethodTests(TestCase):
+  def test_login(self):
+    client = Client()
+    user = User.objects.create_user(username='jerry', password='1234')
+    
+    response = client.post('/purchase/', {'username': 'jerry', 'password': '1234'})
+    
+    self.assertEqual(user.is_authenticated(), True)
+    
+    
+    
+  def test_buy(self):
+    client = Client()
+    user = User.objects.create_user(username='jerry', password='1234')
+    client.login(username='jerry', password='1234')
+    
+    m = Machine.objects.create(location="a", heatGood=True, lastContact=datetime.now(), Admin=user)
+    slot = InventorySlot.objects.create(amount=2, sodaType="MD", Machine=m)
+    u = MachineUser.objects.create(User=user, studentID=1, funds=100)
+    Soda.objects.create(name="MD", sugar=0, calories=0, description="", Slot=slot, cost=100)
+    
+    #buy a soda
+    response = client.post('/purchase/buy', {'slot': 1})
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(SodaTransaction.objects.get(pk=1).amount, 100)
+    self.assertEqual(InventorySlot.objects.get(pk=1).amount, 1)
+    self.assertEqual(MachineUser.objects.get(pk=1).funds, 0)
+    
+    #try to buy a soda with no money
+    response = client.post('/purchase/buy', {'slot': 1})
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(InventorySlot.objects.get(pk=1).amount, 1)
+    self.assertEqual(MachineUser.objects.get(pk=1).funds, 0)
